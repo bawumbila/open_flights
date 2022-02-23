05765b379159ee1f98d1ca56fdc5917117522748
bawumbila <74751215+bawumbila@users.noreply.github.com>
2022-02-23 15:01:24 -0500

    correctiions and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :airlines, param: :slug
+      resources :reviews, only: %i[create destroy]
+      resources :auth, only: %i[create] do
+        collection do
+          post 'password/forgot', to: 'auth#forgot_password'
+          post 'password/reset', to: 'auth#reset_password'
+          get 'me', to: 'auth#logged_in'
+          delete 'logout', to: 'auth#logout'
+        end
+
 end
