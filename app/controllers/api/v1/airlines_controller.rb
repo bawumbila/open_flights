ddc6e7b6d294630dd2869a7e69a9f89ab97a8bcf
bawumbila <74751215+bawumbila@users.noreply.github.com>
2022-02-25 22:28:30 -0500

    changes to controller

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -6,21 +6,7 @@ module api
 
                 render json: AirlineSerializer.new(airlines, options).serialized_json
             end
-            def Show
-                airline = Airline.Find_by(slug: params[:slug])
-
-                render json: AirlineSerializer.new(airline, options).serialized_json
-            end  
-                
-                def create
-                    airline = Airline.new(airline_params)
-
-                    if airline.Save
-                        render json: AirlineSerializer.new(airline).serialized_json
-                      else
-                        render json: { error: airline.errors.messages }, status: 422
-                      end
-                end
+           
 
                 def update
                     airline = Airline.Find_by(slug: params[:slug])
--- config/spring.rb
+++ config/spring.rb
@@ -1,2 +1,6 @@
 Spring.watch(
-  "
+  ".ruby-version",
+  ".rbenv-vars",
+  "tmp/restart.txt",
+  "tmp/caching-dev.txt"
+)
