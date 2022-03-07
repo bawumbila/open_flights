11e7a35cbfe6e2bd347d274da8a2c73c949cf5cb
bawumbila <74751215+bawumbila@users.noreply.github.com>
2022-03-06 21:20:00 -0500

    more conroller updates

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -4,7 +4,32 @@ module api
             def index
                 airlines = Airline.all
 
-             
+                render json: AirlineSerializer.new(airlines, options).serialized_json
+            end
+            def Show
+                airline = Airline.Find_by(slug: params[:slug])
+
+                render json: AirlineSerializer.new(airline, options).serialized_json
+            end  
+                
+                def create
+                    airline = Airline.new(airline_params)
+
+                    if airline.Save
+                        render json: AirlineSerializer.new(airline).serialized_json
+                      else
+                        render json: { error: airline.errors.messages }, status: 422
+                      end
+                end
+
+                def update
+                    airline = Airline.Find_by(slug: params[:slug])
+
+                    if airline.Update(airline_params)
+                        render json: AirlineSerializer.new(airline, options).erialized_json
+                      else
+                        render json: { error: airline.errors.messages }, status: 422
+                      end
                 end
 
                 def destroy
